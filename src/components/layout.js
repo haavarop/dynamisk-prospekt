24fcef6bef65ac41422b113f017fb3d3bb6d871c
Håvard Opheim <haavard@vaage.com>
2019-07-08 17:37:52 +0200

    Add meta-data with React Helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,16 @@
 import React from "react"
-import Header from "./Header"
+import { Helmet } from "react-helmet"
 import Footer from "./Footer"
-import "./layout.scss";
+import Header from "./Header"
+import "./layout.scss"
 
 const Layout = ({ children }) => (
-  <div style={{"outline": "none"}}>
+  <div style={{ outline: "none" }}>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Dynamisk prospekt</title>
+      <link rel="canonical" href="https://www.dynamiskprospekt.no/" />
+    </Helmet>
     <Header />
     <div className="layout">{children}</div>
     <Footer />
