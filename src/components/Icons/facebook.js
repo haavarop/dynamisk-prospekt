698ee4f6d3b807c381ba1f424407830858e9f95e
Håvard Opheim <haavard@vaage.com>
2019-10-05 23:07:28 +0200

    Add SoMe icons

---
+++ src/components/Icons/facebook.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+const FBIcon = () => (
+  <svg
+    width="54"
+    height="54"
+    viewBox="0 0 54 54"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M38.9167 6.41675H32.4167C29.5435 6.41675 26.788 7.55811 24.7564 9.58976C22.7247 11.6214 21.5833 14.3769 21.5833 17.2501V23.7501H15.0833V32.4167H21.5833V49.7501H30.25V32.4167H36.75L38.9167 23.7501H30.25V17.2501C30.25 16.6754 30.4783 16.1243 30.8846 15.718C31.2909 15.3117 31.842 15.0834 32.4167 15.0834H38.9167V6.41675Z"
+      stroke="white"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M40 1H14C6.8203 1 1 6.8203 1 14V40C1 47.1797 6.8203 53 14 53H40C47.1797 53 53 47.1797 53 40V14C53 6.8203 47.1797 1 40 1Z"
+      stroke="white"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+)
+
+export default FBIcon
