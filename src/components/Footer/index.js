fcc79e102ca7966e0a5f85fe5128a4dd4c73fc95
Håvard Opheim <haavard@vaage.com>
2019-10-05 23:07:51 +0200

    Update footer with SoMe and link to contact form

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,34 @@
 import React from "react"
 import LogoIcon from "../Icons/logo"
 import "./footer.scss"
+import { Link } from "gatsby"
+import FBIcon from "../Icons/facebook"
+import InstaIcon from "../Icons/instagram"
+import MAIcon from "../Icons/mittanbud"
 
 const Footer = () => (
-  <div className="footer">
-    <footer>
-      <div className="footer__content">
+  <footer className="footer">
+    <div className="footer__content">
         <LogoIcon />
-        <p>© {new Date().getFullYear()}, Dynamisk Prospekt</p>
+        <div className="footer__links">
+          <Link to="/contact">
+            <h3>Ta kontakt</h3>
+          </Link>
       </div>
-    </footer>
-  </div>
+        <div className="footer__some">
+          <a href="https://mittanbud.no/profil/1046064/dynamisk-prospekt-as/">
+            <MAIcon />
+          </a>
+          <a href="https://www.facebook.com/dynamiskprospekt/">
+            <FBIcon />
+          </a>
+          <a href="https://www.instagram.com/dynamiskprospekt/">
+            <InstaIcon />
+          </a>
+        </div>
+      <div className="footer__links"></div>
+    </div>
+  </footer>
 )
 
 export default Footer
