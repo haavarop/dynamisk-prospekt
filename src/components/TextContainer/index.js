3061402348d431dfb8de8eb155cf8131d6266410
Håvard Opheim <haavard@vaage.com>
2019-10-05 21:58:36 +0200

    Update text container
    * Handle text on left or right side
    * Differentiate between h1 and h2 tag for SEO
    * Render list of bullet points if provided

--- src/components/TextContainer/index.js
+++ src/components/TextContainer/index.js
@@ -1,11 +1,31 @@
+import classNames from "classnames"
 import React from "react"
 import "./textContainer.scss"
 
-const TextContainer = (props) => (
-  <div className="text-container">
-    <h1>{props.title}</h1>
-    <p>{props.text}</p>
-  </div>
-)
+const TextContainer = props => {
+  const renderBulletList = () => {
+    return (
+      <ul>
+        {props.list.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    )
+  }
+
+  return (
+    <div
+      className={classNames(
+        "text-container",
+        props.textRight ? "right-text" : "left-text",
+        props.textTop ? "allign-top" : ""
+      )}
+    >
+      {props.header ? <h1>{props.title}</h1> : <h2>{props.title}</h2>}
+      <p>{props.text}</p>
+      {props.list && renderBulletList()}
+    </div>
+  )
+}
 
 export default TextContainer
